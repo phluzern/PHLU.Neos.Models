25a165535453e3c3c0fe9816b23912d7244fd8df
miegli <michae.egli@phlu.ch>
2016-07-17 15:57:43 +0200

    improved qmpilot service

--- Classes/PHLU/Neos/Models/Service/Contact/ContactService.php
+++ Classes/PHLU/Neos/Models/Service/Contact/ContactService.php
@@ -293,7 +293,7 @@ class ContactService
 
         $updated = false;
 
-        if ($contact->isPersisted() || $this->contactRepository->findByIdentifier($contact->getIdentifier())) {
+        if ($contact->isPersisted()) {
             if ($contact->isHasChanges()) {
                 $contact->setHasChanges(false);
                 $this->contactRepository->update($contact);
