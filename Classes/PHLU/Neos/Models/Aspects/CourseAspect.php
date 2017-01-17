71e544cdbdd02924fcf83cd722d44e7ec2a917cc
miegli <michae.egli@phlu.ch>
2017-01-17 15:36:38 +0100

    added feature evento sync course detail page node creation

--- Classes/PHLU/Neos/Models/Aspects/CourseAspect.php
+++ Classes/PHLU/Neos/Models/Aspects/CourseAspect.php
@@ -8,11 +8,16 @@ use PHLU\Neos\Models\Domain\Model\Course\Study\FurtherEducation\Course;
 use PHLU\Neos\Models\Domain\Repository\CourseRepository;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Persistence\Doctrine\PersistenceManager;
+use TYPO3\Neos\Domain\Repository\SiteRepository;
+use TYPO3\Neos\Domain\Service\ContentContextFactory;
+use TYPO3\Neos\Utility\NodeUriPathSegmentGenerator;
 use TYPO3\TYPO3CR\Domain\Model\NodeData;
 use TYPO3\TYPO3CR\Domain\Repository\NodeDataRepository;
 use TYPO3\TYPO3CR\Domain\Service\NodeTypeManager;
 use TYPO3\Flow\Aop\JoinPointInterface;
 use TYPO3\Neos\Domain\Service\SiteService;
+use TYPO3\Neos\Domain\Model\Site;
+use TYPO3\TYPO3CR\Domain\Model\Node;
 
 /**
  * @Flow\Scope("singleton")
@@ -21,6 +26,19 @@ use TYPO3\Neos\Domain\Service\SiteService;
 class CourseAspect
 {
 
+
+    /**
+     * @Flow\Inject
+     * @var SiteRepository
+     */
+    protected $siteRepository;
+
+
+    /**
+     * @Flow\Inject
+     * @var NodeUriPathSegmentGenerator
+     */
+    protected $nodeUriPathSegmentGenerator;
 
 
     /**
@@ -68,7 +86,11 @@ class CourseAspect
     protected $nodeTypeManager;
 
 
-
+    /**
+     * @Flow\Inject
+     * @var ContentContextFactory
+     */
+    protected $contentContextFactory;
 
     /**
      * @Flow\After("method(PHLU\Neos\Models\Domain\Repository\Course\Study\FurtherEducation\CourseRepository->add|update())")
@@ -87,7 +109,6 @@ class CourseAspect
     {
         $this->findCourseNodesAndUpdate($joinPoint->getMethodArgument('object'));
     }
-
 
 
     /**
@@ -110,7 +131,38 @@ class CourseAspect
             }
         }
 
-        if ($courseid === false && $course->isDeleted() === false ) {
+        $courseDetailId = false;
+        /* @var $baseNode NodeData */
+        $baseNode = $this->nodeDataRepository->findOneByIdentifier($settings['detailContainerNodeId'], $this->workspaceRepository->findByIdentifier('live'));
+
+        foreach ($this->workspaceRepository->findAll() as $workspace) {
+            foreach ($this->nodeDataRepository->findByParentAndNodeTypeRecursively($baseNode->getPath(), $settings['detailNodeType'], $this->workspaceRepository->findByName($workspace)->getFirst()) as $node) {
+                if ($node->getProperty('internalid') == $course->getId()) {
+
+                    $courseDetailId = $course->getId();
+                    $node->setProperty('uriPathSegment', $this->nodeUriPathSegmentGenerator->generateUriPathSegment(null, $course->getTitle()));
+                    $node->setProperty('title', $course->getTitle());
+                    $this->nodeDataRepository->update($node);
+                    $baseNodeHeader = $this->nodeDataRepository->findOneByPath($node->getPath() . "/header", $this->workspaceRepository->findByIdentifier('live'));
+
+                    $baseNodeHeaderHeadlines = $this->nodeDataRepository->findByParentAndNodeType($baseNodeHeader->getPath(),'PHLU.Corporate:Headline',$this->workspaceRepository->findByIdentifier('live'));
+                    foreach ($baseNodeHeaderHeadlines as $headline) {
+                        $headline->setProperty('text',$course->getTitle());
+                        $this->nodeDataRepository->update($headline);
+                    }
+
+                    $baseNodeHeaderTextes = $this->nodeDataRepository->findByParentAndNodeType($baseNodeHeader->getPath(),'PHLU.Corporate:TextPlain',$this->workspaceRepository->findByIdentifier('live'));
+                    foreach ($baseNodeHeaderTextes as $text) {
+                        $text->setProperty('text',$course->getDescription());
+                        $this->nodeDataRepository->update($text);
+                    }
+
+                }
+            }
+        }
+
+
+        if ($courseid === false && $course->isDeleted() === false) {
 
             // create course node
             $baseNode = $this->nodeDataRepository->findOneByIdentifier($settings['containerNodeId'], $this->workspaceRepository->findByIdentifier('live'));
@@ -119,20 +171,64 @@ class CourseAspect
             if ($baseNodeDatabase !== null) {
                 $nodeType = $this->nodeTypeManager->getNodeType($settings['nodeTypeName']);
 
-                if ($this->nodeDataRepository->findOneByPath($baseNodeDatabase->getPath() . "/" . strtolower($settings['repository']).'-' . $course->getId(), $this->workspaceRepository->findByIdentifier('live')) === null) {
-                    $courseNode = $baseNodeDatabase->createNodeData(strtolower($settings['repository']).'-' . $course->getId(), $nodeType);
+                if ($this->nodeDataRepository->findOneByPath($baseNodeDatabase->getPath() . "/" . strtolower($settings['repository']) . '-' . $course->getId(), $this->workspaceRepository->findByIdentifier('live')) === null) {
+                    $courseNode = $baseNodeDatabase->createNodeData(strtolower($settings['repository']) . '-' . $course->getId(), $nodeType);
                     $courseNode->setProperty('id', $course->getId());
                     $this->nodeDataRepository->update($this->updateCourseNode($courseNode, $course));
                 }
             }
 
+        }
+
+        if ($courseDetailId === false && $course->isDeleted() === false) {
+            // create course detail page node
+            /* @var $baseNode NodeData */
+            $baseNodeData = $this->nodeDataRepository->findOneByIdentifier($settings['detailContainerNodeId'], $this->workspaceRepository->findByIdentifier('live'));
+
+            $p = explode("/", $baseNodeData->getContextPath());
+
+            $context = $this->createContext($baseNodeData->getWorkspace()->getName(), $baseNodeData->getDimensions(), array(), $this->siteRepository->findByIdentifier($p[2]));
+
+            /** @var Node $node */
+            $baseNode = new Node(
+                $baseNodeData,
+                $context
+            );
+
+
+            if ($baseNode !== null) {
+
+                $nodeType = $this->nodeTypeManager->getNodeType($settings['detailNodeType']);
+                $courseDetailNode = $baseNode->createNode(strtolower($settings['repository']) . '-' . $course->getId(), $nodeType);
+                $courseDetailNode->setProperty('title', $course->getTitle());
+                $courseDetailNode->setProperty('internalid', $course->getId());
+                $courseDetailNode->setProperty('uriPathSegment', $this->nodeUriPathSegmentGenerator->generateUriPathSegment(null, $course->getTitle()));
+                $this->nodeDataRepository->update($courseDetailNode->getNodeData());
+                $this->persistenceManager->persistAll();
+
+                $baseNodeHeader = $this->nodeDataRepository->findOneByPath($courseDetailNode->getPath() . "/header", $this->workspaceRepository->findByIdentifier('live'));
+
+                $baseNodeHeaderHeadlines = $this->nodeDataRepository->findByParentAndNodeType($baseNodeHeader->getPath(),'PHLU.Corporate:Headline',$this->workspaceRepository->findByIdentifier('live'));
+                foreach ($baseNodeHeaderHeadlines as $headline) {
+                    $headline->setProperty('text',$course->getTitle());
+                    $this->nodeDataRepository->update($headline);
+                }
+
+                $baseNodeHeaderTextes = $this->nodeDataRepository->findByParentAndNodeType($baseNodeHeader->getPath(),'PHLU.Corporate:TextPlain',$this->workspaceRepository->findByIdentifier('live'));
+                foreach ($baseNodeHeaderTextes as $text) {
+                    $text->setProperty('text',$course->getDescription());
+                    $this->nodeDataRepository->update($text);
+                }
+
+
+            }
 
         }
 
+        $this->persistenceManager->persistAll();
 
 
     }
-    
 
 
     /**
@@ -144,26 +240,26 @@ class CourseAspect
     {
 
         /* @var $course \PHLU\Neos\Models\Domain\Model\Course\AbstractCourse */
-        $node->setProperty('title',$course->getTitle());
-        $node->setProperty('nr',$course->getNr());
-        $node->setProperty('description',$course->getDescription());
-        $node->setProperty('ects',$course->getEcts());
-        $node->setProperty('fee',$course->getFee());
-        $node->setProperty('leaders',$course->getLeaders());
-        $node->setProperty('targetgroups',$course->getTargetgroups());
-        $node->setProperty('deleted',$course->isDeleted());
+        $node->setProperty('title', $course->getTitle());
+        $node->setProperty('nr', $course->getNr());
+        $node->setProperty('description', $course->getDescription());
+        $node->setProperty('ects', $course->getEcts());
+        $node->setProperty('fee', $course->getFee());
+        $node->setProperty('leaders', $course->getLeaders());
+        $node->setProperty('targetgroups', $course->getTargetgroups());
+        $node->setProperty('deleted', $course->isDeleted());
 
         switch ($node->getNodeType()->getName()) {
 
             case 'PHLU.Neos.NodeTypes:Course.Study.FurtherEducation':
-            /* @var $course \PHLU\Neos\Models\Domain\Model\Course\Study\FurtherEducation\Course */
-            $node->setProperty('graduation',$course->getGraduation());
-            break;
+                /* @var $course \PHLU\Neos\Models\Domain\Model\Course\Study\FurtherEducation\Course */
+                $node->setProperty('graduation', $course->getGraduation());
+                break;
 
             case 'PHLU.Neos.NodeTypes:Course.Module.FurtherEducation':
-            /* @var $course \PHLU\Neos\Models\Domain\Model\Course\Module\FurtherEducation\Course */
+                /* @var $course \PHLU\Neos\Models\Domain\Model\Course\Module\FurtherEducation\Course */
 
-            break;
+                break;
 
         }
 
@@ -173,7 +269,31 @@ class CourseAspect
 
     }
 
+    /**
+     * Creates a content context for given workspace
+     *
+     * @param string $workspaceName
+     * @param array $dimensions
+     * @param array $targetDimensions
+     * @param Site $currentSite
+     * @return \TYPO3\TYPO3CR\Domain\Service\Context
+     */
+    protected
+    function createContext($workspaceName, $dimensions, $targetDimensions, $currentSite)
+    {
 
+
+        return $this->contentContextFactory->create(array(
+            'workspaceName' => $workspaceName,
+            'currentSite' => $currentSite,
+            'currentSiteNode' => $currentSite,
+            'dimensions' => $dimensions,
+            'targetDimensions' => $targetDimensions,
+            'invisibleContentShown' => false,
+            'inaccessibleContentShown' => false,
+            'removedContentShown' => false
+        ));
+    }
 
 
 }
