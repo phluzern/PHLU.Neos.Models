dee9562580cf9ab3626df3edd37e81681381174e
miegli <michae.egli@phlu.ch>
2017-05-04 12:14:54 +0200

    bug fix

--- Classes/Phlu/Neos/Models/Domain/Repository/Course/Event/FurtherEducation/CourseRepository.php
+++ Classes/Phlu/Neos/Models/Domain/Repository/Course/Event/FurtherEducation/CourseRepository.php
@@ -5,6 +5,7 @@ namespace Phlu\Neos\Models\Domain\Repository\Course\Event\FurtherEducation;
  * This file is part of the Phlu.Neos.Models package.
  */
 
+use Phlu\Neos\Models\Domain\Repository\Course\Event\AbstractEventRepository;
 use Phlu\Neos\Models\Domain\Repository\Course\Module\AbstractModuleRepository;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Persistence\Repository;
@@ -14,7 +15,7 @@ use Neos\Flow\Persistence\QueryInterface;
 /**
  * @Flow\Scope("singleton")
  */
-class CourseRepository extends AbstractModuleRepository
+class CourseRepository extends AbstractEventRepository
 {
 
 
